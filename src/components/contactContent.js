3287edbe3ec6b2b6bbf2a5e1c58e034e46bf6a2c
Kallol Banerjee <kallol.banerjee@tier5.in>
2022-07-15 16:41:17 +0530

    Contact page changes V1.02

--- src/components/contactContent.js
+++ src/components/contactContent.js
@@ -10,6 +10,16 @@ const ContactContent = () => {
     setUsers(data.results)
   }
 
+  // useEffect(() => {
+  //   const lodedData =  window.localStorage.getItem('Contact_Values');
+  //   if (lodedData !== null) setUsers(JSON.parse(users))
+  // } , []);
+
+useEffect(() => {
+  window.localStorage.setItem('Contact_Values', JSON.stringify(users))
+} , [users]);
+
+
   useEffect(() => {
     fetchData()
   }, [])
@@ -24,56 +34,26 @@ const ContactContent = () => {
                 <div className="contactContent">
           {users.map((user, index) => {
             return(
-             
-            //  <tr key={user.gender}>
-            //     <td>{user.name.title} {user.name.first} {user.name.last}</td>
-            //     <td>{user.gender}</td>
-            //     <td>{user.location.street.number} {user.location.street.name}</td>
-            //     <td>{user.location.city}</td>
-            //     <td>{user.location.state}</td>
-            //     <td>{user.location.country}</td>
-            //     <td>{user.location.postcode}</td>
-            //     <td>{user.email}</td>
-            //     <td>{user.phone}</td>
-            //     <td>{user.cell}</td>
-            //     </tr>
-
+           
             <div className="contentWraper">
-
               <div className="leftSecContact">
                 <h2 >Personal Info</h2>
                 <img className="profileImg" src={user.picture.large} alt="" />
-
-
-                <h4 className="nameInfos">{user.name.title} {user.name.first} {user.name.last}</h4>
-             
-
-
+                <h4 className="nameInfos">{user.name.title} {user.name.first} {user.name.last}</h4>   
                 <h4>Email</h4>
                 <p>{user.email}</p>
-
                 <h4>Phone</h4>
                 <p>{user.phone}</p>
-
                 <h4>Cell</h4>
-                <p>{user.cell}</p>
-
-                
-
-
-                
-
+                <p>{user.cell}</p> 
               </div>
 
               <div className="rightSecContact">
                 <h2>Other Info</h2>
-
                 <h4>Gender</h4>
                 <p>{user.gender}</p>
-
                 <h4>Age</h4>
                 <p>{user.dob.age}</p>
-
                 <h4> Address</h4>
                 <p>{user.location.street.number} {user.location.street.name} , {user.location.city} , {user.location.state}
                 <br />
